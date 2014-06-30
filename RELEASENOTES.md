0235c1e576b72e3e6eb9c15846a23c2a32931c42
Christoph Ruegg <git@cdrnet.ch>
2014-06-30 20:52:57 +0200

    Build: set initial version to 0.1.0

--- RELEASENOTES.md
+++ RELEASENOTES.md
@@ -1,2 +1,5 @@
+### 0.1.0 - 2014-06-30
+* Initial version
+
 ### 0.0.0 - 2014-06-27
 * Project Bootstrap
--- src/Spatial/Properties/AssemblyInfo.cs
+++ src/Spatial/Properties/AssemblyInfo.cs
@@ -1,36 +1,22 @@
+using System;
 using System.Reflection;
-using System.Runtime.CompilerServices;
+using System.Resources;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Geometry")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Math.NET Spatial")]
+[assembly: AssemblyDescription("Math.NET Spatial")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Geometry")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Math.NET Project")]
+[assembly: AssemblyProduct("Math.NET Spatial")]
+[assembly: AssemblyCopyright("Copyright © Math.NET Project")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
+[assembly: NeutralResourcesLanguage("en")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b07bca71-81c3-408c-bc3f-2f6b59f6a80e")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
