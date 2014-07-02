6628285e586de19e1f7a520bcf07b98205302eaf
Christoph Ruegg <git@cdrnet.ch>
2014-07-02 15:00:41 +0200

    Clarify current alpha status

--- src/Spatial/Properties/AssemblyInfo.cs
+++ src/Spatial/Properties/AssemblyInfo.cs
@@ -19,4 +19,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.0-alpha")]
