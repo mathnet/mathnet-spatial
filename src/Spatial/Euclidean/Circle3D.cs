d78552b3b42f73308633427865d2d4f7dbae7a43
Mostafa Ali <ma.ali@outlook.com>
2016-02-19 14:46:03 -0700

    Construct circle using 2 points and calculate circumference and area

--- src/Spatial/Euclidean/Circle3D.cs
+++ src/Spatial/Euclidean/Circle3D.cs
@@ -15,5 +15,16 @@ namespace MathNet.Spatial.Euclidean
             this.Axis = axis;
             this.Radius = radius;
         }
+
+        public Circle3D(Point3D p1, Point3D p2, UnitVector3D axis)
+        {
+            this.CenterPoint = Point3D.MidPoint(p1, p2);
+            this.Axis = axis;
+            this.Radius = p1.DistanceTo(CenterPoint);
+        }
+
+        public double Diameter => 2 * Radius;
+        public double Circumference => 2 * Math.PI * Radius;
+        public double Area => Math.PI * Math.Pow(Radius, 2);
     }
 }
