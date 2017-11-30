50322ad2946f4225e84dbeece03c69f28ad644b4
Adam Jones <aquentin2033@outlook.com>
2017-11-30 23:55:46 +0100

    Straightline and Circle

---
+++ src/Spatial/Euclidean/Circle.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MathNet.Spatial.Euclidean
+{
+    /// <summary>
+    /// Describes a standard 2 dimentional circle
+    /// </summary>
+    public class Circle : IEquatable<Circle>
+    {
+        /// <summary>
+        /// The center point of the circle
+        /// </summary>
+        public Point2D Center { get; }
+
+        /// <summary>
+        /// The radius of the circle
+        /// </summary>
+        public double Radius { get; }
+
+        /// <summary>
+        /// Creates a Circle of a given radius from a center point
+        /// </summary>
+        /// <param name="center">The location of the center</param>
+        /// <param name="radius">The radius of the circle</param>
+        public Circle(Point2D center, double radius)
+        {
+            Center = center;
+            Radius = radius;
+        }
+
+        /// <summary>
+        /// Creates a Circle from three points which are not colinnear
+        /// </summary>
+        /// <param name="center">The location of the center</param>
+        /// <param name="radius">The radius of the circle</param>
+        public Circle(Point2D pointA, Point2D pointB, Point2D pointC)
+        {
+            StraightLine lineAB = new StraightLine(pointA, pointB);
+            StraightLine lineBC = new StraightLine(pointB, pointA);
+            var midpointAB = Point2D.MidPoint(pointA, pointB);
+            var midpointBC = Point2D.MidPoint(pointB, pointC);
+            var line1 = lineAB.PerpendicularAt(midpointAB);
+            var line2 = lineBC.PerpendicularAt(midpointBC);
+            var centerpoint = lineAB.Intersection(lineBC);
+            if (!centerpoint.HasValue)
+                throw new ArgumentException("Points cannot form a circle, are they colinnear?");
+            Center = centerpoint.Value;
+            Radius = Center.DistanceTo(pointA);
+        }
+
+        /// <summary>
+        /// Returns the circumference of the circle
+        /// </summary>
+        public double Circumference => 2 * Radius * Math.PI;
+
+        /// <summary>
+        /// Returns the diameter of the circle
+        /// </summary>
+        public double Diameter => 2 * Radius;
+
+        /// <summary>
+        /// Returns the area of the circle
+        /// </summary>
+        public double Area => Radius * Radius * Math.PI;
+
+        /// <summary>
+        /// Returns the tanget at a point on the circle
+        /// </summary>
+        /// <param name="point">a point on the circle</param>
+        /// <returns>A straightline tangent to the circle</returns>
+        public StraightLine TangentAt(Point2D point)
+        {
+            StraightLine radii = new StraightLine(Center, point);
+            return radii.PerpendicularAt(point);
+        }
+
+        /// <summary>
+        /// Returns the normal to the circle passing through point p
+        /// </summary>
+        /// <param name="p">a point on the circle</param>
+        /// <returns>a straightline normal to the circle</returns>
+        public StraightLine NormalAt(Point2D p)
+        {
+            return new StraightLine(Center, p);
+        }
+
+        /// <summary>
+        /// Returns a secant which passes through the points A and B
+        /// </summary>
+        /// <param name="pointA">A point on the circle</param>
+        /// <param name="pointB">A point on the circle</param>
+        /// <returns>A straightline</returns>
+        public StraightLine SecantAt(Point2D pointA, Point2D pointB)
+        {
+            return new StraightLine(pointA, pointB);
+        }
+
+        /// <summary>
+        /// Determines if two circle are equal to each other
+        /// </summary>
+        /// <param name="other">The other circle</param>
+        /// <returns>true if the circles have the same radius and center</returns>
+        public bool Equals(Circle other)
+        {
+            var circle = other as Circle;
+            return (Radius == circle?.Radius && Center == circle?.Center);
+        }
+
+        /// <summary>
+        /// Determines if two objects are equal to each other
+        /// </summary>
+        /// <param name="obj">An object</param>
+        /// <returns>True if the objects are the same</returns>
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is Circle && Equals((Circle)obj);
+        }
+
+        /// <summary>
+        /// Returns the hashocde for a Circle
+        /// </summary>
+        /// <returns>A hashcode</returns>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Center.GetHashCode() * 397) ^ Radius.GetHashCode();
+            }
+        }
+    }
+}
---
+++ src/Spatial/Euclidean/StraightLine.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MathNet.Spatial.Euclidean
+{
+    /// <summary>
+    /// Represents a line conforming to the equation y = mx + b where m is the gradient and b is the y-intercept
+    /// </summary>
+    public class StraightLine : ILine
+    {
+        private double yoffset;
+
+        /// <summary>
+        /// The gradient of the line
+        /// </summary>
+        public double Gradient { get; }
+
+        /// <summary>
+        /// Creates a Line conforming to the equation y = mx + b where point is a point on the line and gradient is the slope of the line
+        /// </summary>
+        /// <param name="point">A point on the line</param>
+        /// <param name="gradient">The gradient of the line</param>
+        public StraightLine(Point2D point, double gradient)
+        {
+            Gradient = gradient;
+            yoffset = point.Y - (Gradient * point.X);
+        }
+
+        /// <summary>
+        /// Creates a Line conforming to the equation y = mx + b which passes through points p1 and p2
+        /// </summary>
+        /// <param name="p1">A point on the line</param>
+        /// <param name="p2">A point on the line</param>
+        public StraightLine(Point2D p1, Point2D p2)
+        {
+            if (p1 == p2)
+                throw new ArgumentException("A line cannot be formed from two identical points");
+            Gradient = (p2.Y - p1.Y) / (p2.X - p1.X);
+
+            if (double.IsInfinity(Gradient))
+                yoffset = double.NaN; // special case vertical line has no y-intercept
+            else
+                yoffset = p1.Y - (Gradient * p1.X);
+        }
+
+        /// <summary>
+        /// Creates a Line conforming to the equation y = mx + b which passes through the points provided
+        /// </summary>
+        /// <param name="points">The points though which the line should pass</param>
+        /// <exception cref="ArgumentException">Thrown when less than 2 points are provided</exception>
+        /// <exception cref="ArgumentException">Thrown when points are not collinear</exception>
+        public StraightLine(IEnumerable<Point2D> points) : this(points.ToArray())
+        {
+        }
+
+        /// <summary>
+        /// Creates a Line conforming to the equation y = mx + b which passes through the points provided
+        /// </summary>
+        /// <param name="points">The points though which the line should pass</param>
+        /// <exception cref="ArgumentException">Thrown when less than 2 points are provided</exception>
+        /// <exception cref="ArgumentException">Thrown when points are not collinear</exception>
+        public StraightLine(Point2D[] points)
+        {
+            if (points.Length < 2)
+                throw new ArgumentException("A line must be constructed from at least two points");
+
+            Gradient = (points[1].Y - points[0].Y) / (points[1].X - points[0].X);
+            yoffset = points[0].Y - (Gradient * points[0].X);
+            for (int i = 2; i < points.Length; i++)
+            {
+                if (!IsOnLine(points[i]))
+                {
+                    throw new ArgumentException("The points provided are not collinear");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Determines if the line is horizontal
+        /// </summary>
+        /// <returns>True if the line is horizontal</returns>
+        public bool IsHorizontal()
+        {
+            return (Gradient == 0);
+        }
+
+        /// <summary>
+        /// Determines if the line is vertical
+        /// </summary>
+        /// <returns>returns true if the line is vertical</returns>
+        public bool IsVertical()
+        {
+            return (double.IsInfinity(Gradient));
+        }
+
+        /// <summary>
+        /// Tests if the other line is parallel to this one
+        /// </summary>
+        /// <returns>Returns true if lines are parallel</returns>
+        public bool IsParallel(ILine otherLine)
+        {
+            var secondLine = otherLine as StraightLine;
+            return (secondLine?.Gradient == Gradient);
+        }
+
+        /// <summary>
+        /// Finds the midpoint of two points along the current line
+        /// </summary>
+        /// <param name="pointA">a point on the line</param>
+        /// <param name="pointB">a point on the line</param>
+        /// <returns>A point midway between the two provided points</returns>
+        public Point2D MidPoint(Point2D pointA, Point2D pointB)
+        {
+            return new Point2D((pointA.X + pointB.X) / 2, (pointA.Y + pointB.Y) / 2);
+        }
+
+        /// <summary>
+        /// Returns a line perpendicular to this line going through point p
+        /// </summary>
+        /// <param name="p">The point on the line which it passes through</param>
+        /// <returns>A perpendicular straightline</returns>
+        public StraightLine PerpendicularAt(Point2D p)
+        {
+            return new StraightLine(p, -1 / Gradient);
+        }
+
+        /// <summary>
+        /// Find the intersection between two lines
+        /// </summary>
+        /// <param name="line">The line to intersect with</param>
+        /// <returns>The point of intersection or null if the lines are parallel</returns>
+        public Point2D? Intersection(StraightLine line)
+        {
+            if (IsParallel(line))
+                return null;
+            var denominator = Gradient - line.Gradient;
+            var x = line.yoffset - yoffset / denominator;
+            var y = Gradient * x + yoffset;
+            return new Point2D(x, y);
+        }
+
+        /// <summary>
+        /// Returns the closest point on the line to the given point.
+        /// </summary>
+        /// <param name="p">The point that the returned point is the closest point on the line to</param>
+        /// <returns></returns>
+        public Point2D ClosestPointTo(Point2D p)
+        {
+            StraightLine line = new StraightLine(p, Gradient);
+            var perpline = line.PerpendicularAt(p);
+            return this.Intersection(perpline).Value;
+        }
+
+        /// <summary>
+        /// Determines if a given point is on this line
+        /// </summary>
+        /// <param name="point">The point to be checked</param>
+        /// <returns>True if the point is on this line</returns>
+        public bool IsOnLine(Point2D point)
+        {
+            return IsOnLine(point.X, point.Y);
+        }
+
+        /// <summary>
+        /// Determines if a set of coordinates (x,y) is on this line
+        /// </summary>
+        /// <param name="x">the x coordinate</param>
+        /// <param name="y">the y coordinate</param>
+        /// <returns>True if the point is on the line</returns>
+        public bool IsOnLine(double x, double y)
+        {
+            return (y == (Gradient * x) + yoffset);
+        }
+
+        /// <summary>
+        /// Returns the list of points which lie on a vertical line at coordinate x
+        /// </summary>
+        /// <param name="x">the x coordinate</param>
+        /// <returns>A list of points</returns>
+        public IEnumerable<Point2D> XIntercept(double x = 0)
+        {
+            return new List<Point2D>() { new Point2D(x, (Gradient * x) + yoffset) };
+        }
+
+        /// <summary>
+        /// Returns the list of points which lie on a horizontal line at coordinate y
+        /// </summary>
+        /// <param name="y">the y coordinate</param>
+        /// <returns>A list of points</returns>
+        public IEnumerable<Point2D> YIntercept(double y = 0)
+        {
+            return new List<Point2D>() { new Point2D(((y - yoffset) / Gradient), y) };
+        }
+
+        /// <summary>
+        /// Determines if two lines are equal to each other
+        /// </summary>
+        /// <param name="other">The other line</param>
+        /// <returns>true if the lines have the same equation</returns>
+        public bool Equals(ILine other)
+        {
+            var sline = other as StraightLine;
+            return (Gradient == sline?.Gradient && yoffset == sline?.yoffset);
+        }
+
+        /// <summary>
+        /// Determines if two objects are equal to each other
+        /// </summary>
+        /// <param name="obj">An object</param>
+        /// <returns>True if the objects are the same</returns>
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is StraightLine && Equals((StraightLine)obj);
+        }
+
+        /// <summary>
+        /// Returns the hashocde for a Straightline
+        /// </summary>
+        /// <returns>A hashcode</returns>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Gradient.GetHashCode() * 397) ^ yoffset.GetHashCode();
+            }
+        }
+    }
+}
