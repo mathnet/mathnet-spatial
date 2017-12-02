d683e8f0ba11d61a64a1650916bc37c7ebfd6a7f
Adam Jones <aquentin2033@outlook.com>
2017-12-02 14:34:39 +0100

    Circle2D

---
+++ src/Spatial/Euclidean/Circle2D.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MathNet.Spatial.Euclidean
+{
+    /// <summary>
+    /// Describes a standard 2 dimentional circle
+    /// </summary>
+    public struct Circle2D : IEquatable<Circle2D>
+    {
+        /// <summary>
+        /// Creates a Circle of a given radius from a center point
+        /// </summary>
+        /// <param name="center">The location of the center</param>
+        /// <param name="radius">The radius of the circle</param>
+        public Circle2D(Point2D center, double radius)
+        {
+            Center = center;
+            Radius = radius;
+        }
+
+        /// <summary>
+        /// The center point of the circle
+        /// </summary>
+        public Point2D Center { get; }
+
+        /// <summary>
+        /// The radius of the circle
+        /// </summary>
+        public double Radius { get; }
+
+        /// <summary>
+        /// Returns the circumference of the circle
+        /// </summary>
+        public double Circumference => 2 * Radius * Math.PI;
+
+        /// <summary>
+        /// Returns the diameter of the circle
+        /// </summary>
+        public double Diameter => 2 * Radius;
+
+        /// <summary>
+        /// Returns the area of the circle
+        /// </summary>
+        public double Area => Radius * Radius * Math.PI;
+
+        public static bool operator ==(Circle2D left, Circle2D right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Circle2D left, Circle2D right)
+        {
+            return !left.Equals(right);
+        }
+
+        /// <summary>
+        /// Determines if two circle are equal to each other
+        /// </summary>
+        /// <param name="other">The other circle</param>
+        /// <returns>true if the circles have the same radius and center</returns>
+        public bool Equals(Circle2D other)
+        {
+            return Radius == other.Radius && Center == other.Center;
+        }
+
+        /// <summary>
+        /// Determines if two objects are equal to each other
+        /// </summary>
+        /// <param name="obj">An object</param>
+        /// <returns>True if the objects are the same</returns>
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            return obj is Circle2D && Equals((Circle2D)obj);
+        }
+
+        /// <summary>
+        /// Returns the hashocde for a Circle
+        /// </summary>
+        /// <returns>A hashcode</returns>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Center.GetHashCode() * 397) ^ Radius.GetHashCode();
+            }
+        }
+    }
+}
---
+++ src/SpatialUnitTests/Euclidean/Circle2DTests.cs
@@ -0,0 +1,33 @@
+﻿namespace MathNet.Spatial.UnitTests.Euclidean
+{
+    using System;
+    using MathNet.Spatial.Euclidean;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Circle2DTests
+    {
+        [TestCase("0, 0", 2.5)]
+        [TestCase("2, -4", 4.7)]
+        public void CircleCenterRadius(string p1s, double radius)
+        {
+            var center = Point2D.Parse(p1s);
+            var circle = new Circle2D(center, radius);
+            Assert.AreEqual(2 * radius, circle.Diameter, double.Epsilon);
+            Assert.AreEqual(2 * Math.PI * radius, circle.Circumference, double.Epsilon);
+            Assert.AreEqual(Math.PI * radius * radius, circle.Area, double.Epsilon);
+        }
+
+        [TestCase("0, 0", 1)]
+        [TestCase("2, -4", 4.7)]
+        public void CircleEquality(string center, double radius)
+        {
+            var cp = Point2D.Parse(center);
+            Circle2D c = new Circle2D(cp, radius);
+            Circle2D c2 = new Circle2D(cp, radius);
+            Assert.True(c == c2);
+            Assert.True(c.Equals(c2));
+        }
+
+    }
+}
