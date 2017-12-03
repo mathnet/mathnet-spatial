06835696df5c4b42a6c1ba284cfd094820157041
Johan Larsson <johan_gavle1@hotmail.com>
2017-12-04 00:01:29 +0100

    Move to nested.

--- src/SpatialUnitTests/AssertXmlTests.cs
+++ src/SpatialUnitTests/AssertXmlTests.cs
@@ -1,5 +1,10 @@
 namespace MathNet.Spatial.UnitTests
 {
+    using System.Globalization;
+    using System.Xml;
+    using System.Xml.Linq;
+    using System.Xml.Schema;
+    using System.Xml.Serialization;
     using NUnit.Framework;
 
     public class AssertXmlTests
@@ -20,6 +25,41 @@ namespace MathNet.Spatial.UnitTests
             var roundTrip = AssertXml.DataContractRoundTrip(dummy, @"<XmlSerializableDummy Age=""14""><Name>Meh</Name></XmlSerializableDummy>");
             Assert.AreEqual(dummy.Name, roundTrip.Name);
             Assert.AreEqual(dummy.Age, roundTrip.Age);
+        }
+
+        public class XmlSerializableDummy : IXmlSerializable
+        {
+            private readonly string name;
+
+            private XmlSerializableDummy()
+            {
+            }
+
+            public XmlSerializableDummy(string name, int age)
+            {
+                this.Age = age;
+                this.name = name;
+            }
+
+            public string Name => this.name;
+
+            public int Age { get; set; }
+
+            public XmlSchema GetSchema() { return null; }
+
+            public void ReadXml(XmlReader reader)
+            {
+                var e = (XElement)XNode.ReadFrom(reader);
+                this.Age = XmlConvert.ToInt32(e.Attribute("Age").Value);
+                var name = e.ReadAttributeOrElement("Name");
+                XmlExt.WriteValueToReadonlyField(this, name, () => this.name);
+            }
+
+            public void WriteXml(XmlWriter writer)
+            {
+                writer.WriteAttributeString("Age", this.Age.ToString(CultureInfo.InvariantCulture));
+                writer.WriteElementString("Name", this.Name);
+            }
         }
     }
 }
