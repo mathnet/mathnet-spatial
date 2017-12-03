163fd69f720be6aae98e64d8eaf67efeceac7b83
JohanLarsson <johan_gavle1@hotmail.com>
2017-12-03 16:13:02 +0100

    Amend.

--- src/Spatial/Internals/Text.cs
+++ src/Spatial/Internals/Text.cs
@@ -1,11 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace MathNet.Spatial.Internals
+﻿namespace MathNet.Spatial.Internals
 {
-    class Parse
+    using System.Globalization;
+    using System.Text.RegularExpressions;
+
+    internal static class Text
     {
+        private const string DoublePattern = @"[+-]?\d*(?:[.,]\d+)?(?:[eE][+-]?\d+)?";
+        private const string SeparatorPattern = @" *[,; ] *";
+        private static readonly string Vector2DPattern = $@"^ *\(?(?<x>{DoublePattern}){SeparatorPattern}(?<y>{DoublePattern})?\)? *$";
+
+        internal static bool TryParse2D(string text, out double x, out double y)
+        {
+            x = 0;
+            y = 0;
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return false;
+            }
+
+            var match = Regex.Match(text, Vector2DPattern);
+            if (!match.Success ||
+                match.Groups.Count != 2 ||
+                match.Groups[0].Captures.Count != 1 ||
+                match.Groups[1].Captures.Count != 1)
+            {
+                return false;
+            }
+
+            return TryParseDouble(match.Groups["x"], out x) &&
+                   TryParseDouble(match.Groups["y"], out y);
+        }
+
+        private static bool TryParseDouble(Group @group, out double result)
+        {
+            if (@group.Captures.Count != 1)
+            {
+                result = 0;
+                return false;
+            }
+
+            return TryParseDouble(@group.Value, out result);
+        }
+
+        private static bool TryParseDouble(string s, out double result)
+        {
+            return double.TryParse(s.Replace(',', '.'), NumberStyles.Float, CultureInfo.InvariantCulture, out result);
+        }
     }
 }
