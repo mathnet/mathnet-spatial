eb22acf02f7d3536cae002bbf61db834b51ee8de
Adam Jones <aquentin2033@outlook.com>
2017-12-06 00:17:44 +0100

    Added FromPoints to Circle2D

--- src/Spatial/Euclidean/Circle2D.cs
+++ src/Spatial/Euclidean/Circle2D.cs
@@ -44,14 +44,57 @@
         /// </summary>
         public double Area => this.Radius * this.Radius * Math.PI;
 
+        /// <summary>
+        /// Returns a value that indicates whether each pair of elements in two specified circles is equal.
+        /// </summary>
+        /// <param name="left">The first circle to compare.</param>
+        /// <param name="right">The second circle to compare.</param>
+        /// <returns>True if the circles are the same; otherwise false.</returns>
         public static bool operator ==(Circle2D left, Circle2D right)
         {
             return left.Equals(right);
         }
 
+        /// <summary>
+        /// Returns a value that indicates whether any pair of elements in two specified circles is not equal.
+        /// </summary>
+        /// <param name="left">The first circle to compare.</param>
+        /// <param name="right">The second circle to compar.e</param>
+        /// <returns>True if the circles are different; otherwise false.</returns>
         public static bool operator !=(Circle2D left, Circle2D right)
         {
             return !left.Equals(right);
+        }
+
+        /// <summary>
+        /// Creates a <see cref="Circle2D"/> circle from three points which lie along its circumference.
+        /// Points may not be collinear
+        /// </summary>
+        /// <param name="pointA">The first point on the circle.</param>
+        /// <param name="pointB">The second point on the circle.</param>
+        /// <param name="pointC">The third point on the circle.</param>
+        /// <returns>A Circle which is defined by the three specified points</returns>
+        /// <exception cref="ArgumentException">An exception is thrown if no possible circle can be formed from the points</exception>
+        public static Circle2D FromPoints(Point2D pointA, Point2D pointB, Point2D pointC)
+        {
+            var midpointAB = Point2D.MidPoint(pointA, pointB);
+            var midpointBC = Point2D.MidPoint(pointB, pointC);
+            double gradientAB = (pointB.Y - pointA.Y) / (pointB.X - pointA.X);
+            double gradientBC = (pointC.Y - pointB.Y) / (pointC.X - pointB.X);
+            double gradientl1 = -1 / gradientAB;
+            double gradientl2 = -1 / gradientBC;
+            double denominator = gradientl2 - gradientl1;
+            double nominator = midpointAB.Y - (gradientl1 * midpointAB.X) + (gradientl2 * midpointBC.X) - midpointBC.Y;
+            double centerX = nominator / denominator;
+            double centerY = (gradientl1 * (centerX - midpointAB.X)) + midpointAB.Y;
+            Point2D center = new Point2D(centerX, centerY);
+
+            if (double.IsNaN(center.X) || double.IsNaN(center.Y) || double.IsInfinity(center.X) || double.IsInfinity(center.Y))
+            {
+                throw new ArgumentException("Points cannot form a circle, are they colinnear?");
+            }
+
+            return new Circle2D(center, center.DistanceTo(pointA));
         }
 
         /// <inheritdoc />
