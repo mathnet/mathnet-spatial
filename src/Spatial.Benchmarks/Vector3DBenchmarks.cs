678435494f44f03f6e5e289321136194add304e9
JohanLarsson <johan_gavle1@hotmail.com>
2017-12-06 08:43:44 +0100

    Rerun benchmarks.

--- src/Spatial.Benchmarks/Vector3DBenchmarks.cs
+++ src/Spatial.Benchmarks/Vector3DBenchmarks.cs
@@ -1,12 +1,20 @@
 ﻿namespace Spatial.Benchmarks
 {
+    using System.Runtime.CompilerServices;
     using BenchmarkDotNet.Attributes;
     using MathNet.Spatial.Euclidean;
+    using MathNet.Spatial.Units;
 
     public class Vector3DBenchmarks
     {
         private static readonly Vector3D V1 = new Vector3D(1, 2, 3);
         private static readonly Vector3D V2 = new Vector3D(3, 4, 5);
+
+        [Benchmark]
+        public double Length()
+        {
+            return V1.Length;
+        }
 
         [Benchmark]
         public UnitVector3D Normalize()
@@ -18,6 +26,80 @@
         public double DotProduct()
         {
             return V1.DotProduct(V2);
+        }
+
+        [Benchmark]
+        public double OperatorMultiplyVector()
+        {
+            return V1 * V2;
+        }
+
+        [Benchmark]
+        public Vector3D OperatorAdd()
+        {
+            return V1 + V2;
+        }
+
+        [Benchmark]
+        public Vector3D ScaleBy()
+        {
+            return V1.ScaleBy(2);
+        }
+
+        [Benchmark]
+        public Vector3D OperatorMultiplyDouble()
+        {
+            return 2 * V1;
+        }
+
+        [Benchmark]
+        public bool IsParallelToAngle()
+        {
+            return V1.IsParallelTo(V2, Angle.FromRadians(0));
+        }
+
+        [Benchmark]
+        public bool IsParallelToDouble()
+        {
+            return V1.IsParallelTo(V2, 0);
+        }
+
+        [Benchmark]
+        public Vector3D Rotate()
+        {
+            return V1.Rotate(UnitVector3D.XAxis, Angle.FromRadians(1));
+        }
+
+        [Benchmark]
+        public Angle AngleTo()
+        {
+            return V1.AngleTo(V2);
+        }
+
+        [Benchmark]
+        public Angle SignedAngleTo()
+        {
+            return V1.SignedAngleTo(V2, UnitVector3D.XAxis);
+        }
+
+        [Benchmark]
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        public bool OperatorEquals()
+        {
+            // ReSharper disable once EqualExpressionComparison
+#pragma warning disable CS1718 // Comparison made to same variable
+            return V1 == V1;
+#pragma warning restore CS1718 // Comparison made to same variable
+        }
+
+        [Benchmark]
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        public bool Equals()
+        {
+            // ReSharper disable once EqualExpressionComparison
+#pragma warning disable CS1718 // Comparison made to same variable
+            return V1.Equals(V1);
+#pragma warning restore CS1718 // Comparison made to same variable
         }
     }
 }
--- src/Spatial.Benchmarks/Vector3DBenchmarks.md
+++ src/Spatial.Benchmarks/Vector3DBenchmarks.md
@@ -8,7 +8,19 @@ Frequency=3410117 Hz, Resolution=293.2451 ns, Timer=TSC
 
 
 ```
-|     Method |        Mean |     Error |    StdDev |      Median | Allocated |
-|----------- |------------:|----------:|----------:|------------:|----------:|
-|  Normalize | 143.9735 ns | 3.1835 ns | 7.8687 ns | 138.1000 ns |       0 B |
-| DotProduct |   0.9593 ns | 0.3162 ns | 0.5621 ns |   0.5660 ns |       0 B |
+|                 Method |         Mean |      Error |     StdDev |        Median |  Gen 0 | Allocated |
+|----------------------- |-------------:|-----------:|-----------:|--------------:|-------:|----------:|
+|                 Length |     1.385 ns |  0.3264 ns |  0.6209 ns |     1.5461 ns |      - |       0 B |
+|              Normalize |   142.580 ns |  3.1340 ns |  6.6107 ns |   140.3477 ns |      - |       0 B |
+|             DotProduct |     1.086 ns |  0.3295 ns |  0.7636 ns |     0.9842 ns |      - |       0 B |
+| OperatorMultiplyVector |     1.011 ns |  0.1969 ns |  0.1644 ns |     1.0081 ns |      - |       0 B |
+|            OperatorAdd |     2.615 ns |  0.4627 ns |  0.4752 ns |     2.4897 ns |      - |       0 B |
+|                ScaleBy |     1.557 ns |  0.0487 ns |  0.0380 ns |     1.5513 ns |      - |       0 B |
+| OperatorMultiplyDouble |     1.420 ns |  0.0053 ns |  0.0045 ns |     1.4193 ns |      - |       0 B |
+|      IsParallelToAngle |   362.248 ns |  7.1844 ns | 16.2165 ns |   363.1630 ns |      - |       0 B |
+|     IsParallelToDouble |   321.965 ns |  6.4002 ns | 11.5408 ns |   323.9992 ns |      - |       0 B |
+|                 Rotate | 1,318.857 ns | 26.5503 ns | 61.0037 ns | 1,316.5528 ns | 0.4425 |    2320 B |
+|                AngleTo |   342.210 ns |  7.1548 ns | 10.4874 ns |   335.2963 ns |      - |       0 B |
+|          SignedAngleTo |   928.628 ns | 18.8798 ns | 47.0172 ns |   900.6708 ns |      - |       0 B |
+|         OperatorEquals |     4.704 ns |  0.1312 ns |  0.2298 ns |     4.7116 ns |      - |       0 B |
+|                 Equals |    14.277 ns |  0.3182 ns |  0.6712 ns |    14.3222 ns |      - |       0 B |
