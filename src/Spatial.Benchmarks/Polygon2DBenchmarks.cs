343a05b5388b448e003d3a62a86f1e8c8e3219bf
JohanLarsson <johan_gavle1@hotmail.com>
2017-12-09 21:58:00 +0100

    Polygon2DBenchmarks

---
+++ src/Spatial.Benchmarks/Polygon2DBenchmarks.cs
@@ -0,0 +1,34 @@
+﻿namespace Spatial.Benchmarks
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using BenchmarkDotNet.Attributes;
+    using MathNet.Spatial.Euclidean;
+
+    public class Polygon2DBenchmarks
+    {
+        private static readonly Random Random = new Random();
+        private static readonly IReadOnlyList<Point2D> Point2D50 = Enumerable.Repeat<int>(0, 50).Select(_ => new Point2D(Random.Next(), Random.Next())).ToList();
+        private static readonly IReadOnlyList<Point2D> Point2D500 = Enumerable.Repeat<int>(0, 50).Select(_ => new Point2D(Random.Next(), Random.Next())).ToList();
+        private static readonly IReadOnlyList<Point2D> Point2D5000 = Enumerable.Repeat<int>(0, 50).Select(_ => new Point2D(Random.Next(), Random.Next())).ToList();
+
+        [Benchmark]
+        public Polygon2D GetConvexHullFromPoints50()
+        {
+            return Polygon2D.GetConvexHullFromPoints(Point2D50);
+        }
+
+        [Benchmark]
+        public Polygon2D GetConvexHullFromPoints500()
+        {
+            return Polygon2D.GetConvexHullFromPoints(Point2D500);
+        }
+
+        [Benchmark]
+        public Polygon2D GetConvexHullFromPoints5000()
+        {
+            return Polygon2D.GetConvexHullFromPoints(Point2D5000);
+        }
+    }
+}
