518a785a86710d437f73a66126cea8763f44e43e
JohanLarsson <johan_gavle1@hotmail.com>
2017-12-09 22:01:35 +0100

    Use multiples of 10.

--- src/Spatial.Benchmarks/Polygon2DBenchmarks.cs
+++ src/Spatial.Benchmarks/Polygon2DBenchmarks.cs
@@ -9,26 +9,33 @@
     public class Polygon2DBenchmarks
     {
         private static readonly Random Random = new Random();
-        private static readonly IReadOnlyList<Point2D> Point2D50 = Enumerable.Repeat(0, 50).Select(_ => new Point2D(Random.Next(), Random.Next())).ToList();
-        private static readonly IReadOnlyList<Point2D> Point2D500 = Enumerable.Repeat(0, 500).Select(_ => new Point2D(Random.Next(), Random.Next())).ToList();
-        private static readonly IReadOnlyList<Point2D> Point2D5000 = Enumerable.Repeat(0, 500).Select(_ => new Point2D(Random.Next(), Random.Next())).ToList();
+        private static readonly IReadOnlyList<Point2D> Point2D10 = Enumerable.Repeat(0, 10).Select(_ => new Point2D(Random.Next(), Random.Next())).ToList();
+        private static readonly IReadOnlyList<Point2D> Point2D100 = Enumerable.Repeat(0, 100).Select(_ => new Point2D(Random.Next(), Random.Next())).ToList();
+        private static readonly IReadOnlyList<Point2D> Point2D1000 = Enumerable.Repeat(0, 1000).Select(_ => new Point2D(Random.Next(), Random.Next())).ToList();
+        private static readonly IReadOnlyList<Point2D> Point2D10000 = Enumerable.Repeat(0, 10000).Select(_ => new Point2D(Random.Next(), Random.Next())).ToList();
 
         [Benchmark]
-        public Polygon2D GetConvexHullFromPoints50()
+        public Polygon2D GetConvexHullFromPoints10()
         {
-            return Polygon2D.GetConvexHullFromPoints(Point2D50);
+            return Polygon2D.GetConvexHullFromPoints(Point2D10);
         }
 
         [Benchmark]
-        public Polygon2D GetConvexHullFromPoints500()
+        public Polygon2D GetConvexHullFromPoints100()
         {
-            return Polygon2D.GetConvexHullFromPoints(Point2D500);
+            return Polygon2D.GetConvexHullFromPoints(Point2D100);
         }
 
         [Benchmark]
-        public Polygon2D GetConvexHullFromPoints5000()
+        public Polygon2D GetConvexHullFromPoints1000()
         {
-            return Polygon2D.GetConvexHullFromPoints(Point2D5000);
+            return Polygon2D.GetConvexHullFromPoints(Point2D1000);
+        }
+
+        [Benchmark]
+        public Polygon2D GetConvexHullFromPoints10000()
+        {
+            return Polygon2D.GetConvexHullFromPoints(Point2D10000);
         }
     }
 }
