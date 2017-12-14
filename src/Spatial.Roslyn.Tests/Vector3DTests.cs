6aa265cdb6c76c8c6aed2364999ea90c122a60a0
JohanLarsson <johan_gavle1@hotmail.com>
2017-12-14 13:49:46 +0100

    Codefix replace Vector3D.Rotate with angle unit.

---
+++ src/Spatial.Roslyn.Tests/Vector3DTests.cs
@@ -0,0 +1,88 @@
+﻿namespace Spatial.Roslyn.Tests
+{
+    using Gu.Roslyn.Asserts;
+    using NUnit.Framework;
+    using SpatialAnalyzers;
+
+    public class Vector3DTests
+    {
+        // ReSharper disable once InconsistentNaming
+        private static readonly ExpectedDiagnostic CS0618 = ExpectedDiagnostic.Create("CS0618");
+
+        [Test]
+        public void ReplaceRotateWithDegreesWithRotateWithAngleFromDegrees()
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using MathNet.Spatial.Euclidean;
+    using MathNet.Spatial.Units;
+
+    class Foo
+    {
+        public Foo()
+        {
+            var v = new Vector3D(1, 0, 0);
+            var about = UnitVector3D.Create(0, 1, 0);
+            var rotated = v.Rotate(about, 1.2, AngleUnit.Degrees);
+        }
+    }
+}";
+            var fixedCode = @"
+namespace RoslynSandbox
+{
+    using MathNet.Spatial.Euclidean;
+    using MathNet.Spatial.Units;
+
+    class Foo
+    {
+        public Foo()
+        {
+            var v = new Vector3D(1, 0, 0);
+            var about = UnitVector3D.Create(0, 1, 0);
+            var rotated = v.Rotate(about, Angle.FromDegrees(1.2));
+        }
+    }
+}";
+            AnalyzerAssert.CodeFix<UpdateCodeFix>(CS0618, testCode, fixedCode);
+        }
+
+        [Test]
+        public void ReplaceRotateWithRadiansWithRotateWithAngleFromRadians()
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using MathNet.Spatial.Euclidean;
+    using MathNet.Spatial.Units;
+
+    class Foo
+    {
+        public Foo()
+        {
+            var v = new Vector3D(1, 0, 0);
+            var about = UnitVector3D.Create(0, 1, 0);
+            var rotated = v.Rotate(about, 1.2, AngleUnit.Radians);
+        }
+    }
+}";
+            var fixedCode = @"
+namespace RoslynSandbox
+{
+    using MathNet.Spatial.Euclidean;
+    using MathNet.Spatial.Units;
+
+    class Foo
+    {
+        public Foo()
+        {
+            var v = new Vector3D(1, 0, 0);
+            var about = UnitVector3D.Create(0, 1, 0);
+            var rotated = v.Rotate(about, Angle.FromRadians(1.2));
+        }
+    }
+}";
+            AnalyzerAssert.CodeFix<UpdateCodeFix>(CS0618, testCode, fixedCode);
+        }
+    }
+}
