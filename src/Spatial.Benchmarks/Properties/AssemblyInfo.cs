f886337db85413ad03efa3b9fee38d5a39d8d79d
Adam Jones <aquentin2033@outlook.com>
2017-12-20 13:43:24 +0100

    benchmark build + stylecop config fixes + file cleanup

--- src/Spatial.Benchmarks/Properties/AssemblyInfo.cs
+++ src/Spatial.Benchmarks/Properties/AssemblyInfo.cs
@@ -1,16 +1,4 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Spatial.Benchmarks")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Johan Larsson")]
-[assembly: AssemblyProduct("Spatial.Benchmarks")]
-[assembly: AssemblyCopyright("Copyright © Johan Larsson 2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("3d8a4a0d-0282-4c17-8673-9fcfb52728f6")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: BenchmarkDotNet.Attributes.Config(typeof(Spatial.Benchmarks.Properties.MemoryDiagnoserConfig))]
