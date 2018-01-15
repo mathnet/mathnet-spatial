0ee28281cd8bb3a64bfed0a4e6eb8d107e3d59fd
Adam Jones <aquentin2033@outlook.com>
2018-01-15 20:47:47 +0100

    Json Serialization Implementation

---
+++ src/Spatial.Serialization.Json/SpatialJsonConverter.cs
@@ -0,0 +1,49 @@
+﻿namespace MathNet.Spatial.Serialization.Json
+{
+    using System;
+    using MathNet.Spatial.Serialization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
+    /// <summary>
+    /// Provides support for json serialization of spatial types
+    /// </summary>
+    public class SpatialJsonConverter : JsonConverter
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SpatialJsonConverter"/> class.
+        /// </summary>
+        public SpatialJsonConverter()
+        {
+        }
+
+        /// <inheritdoc />
+        public override bool CanRead => true;
+
+        /// <inheritdoc />
+        public override bool CanWrite => true;
+
+        /// <inheritdoc />
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            Type surrogateType = SpatialSerialization.GetSurrogateType(objectType);
+            JToken token = JToken.Load(reader);
+            object surrogateData = token.ToObject(surrogateType, serializer);
+            return SpatialSerialization.GetDeserializedObject(surrogateData);
+        }
+
+        /// <inheritdoc />
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            object surrogate = SpatialSerialization.GetObjectToSerialize(value);
+            JToken t = JToken.FromObject(surrogate, serializer);
+            t.WriteTo(writer);
+        }
+
+        /// <inheritdoc />
+        public override bool CanConvert(Type objectType)
+        {
+            return SpatialSerialization.CanConvert(objectType);
+        }
+    }
+}
--- src/SpatialUnitTests/Serialization/JsonTests.cs
+++ src/SpatialUnitTests/Serialization/JsonTests.cs
@@ -1,7 +1,10 @@
-﻿namespace MathNet.Spatial.Serialization.Xml.UnitTests
+﻿namespace MathNet.Spatial.Serialization.UnitTests
 {
+#if NETCOREAPP1_1 == false
+    using System.Diagnostics;
     using System.Linq;
     using MathNet.Spatial.Euclidean;
+    using MathNet.Spatial.Serialization.Json;
     using MathNet.Spatial.Units;
     using MathNet.Spatial.UnitTests;
     using Newtonsoft.Json;
@@ -11,7 +14,6 @@
     {
         private const double Tolerance = 1e-6;
 
-        [Explicit("fix later")]
         [TestCase("15 °")]
         public void AngleJson(string vs)
         {
@@ -20,7 +22,6 @@
             Assert.AreEqual(angle.Radians, roundTrip.Radians, Tolerance);
         }
 
-        [Explicit("fix later")]
         [Test]
         public void Point2DJson()
         {
@@ -29,7 +30,6 @@
             Assert.AreEqual(p, result);
         }
 
-        [Explicit("fix later")]
         [Test]
         public void Point3DJson()
         {
@@ -38,7 +38,6 @@
             Assert.AreEqual(p, result);
         }
 
-        [Explicit("fix later")]
         [Test]
         public void QuaternionJson()
         {
@@ -56,7 +55,6 @@
             Assert.AreEqual(eulerAngles, result);
         }
 
-        [Explicit("fix later")]
         [TestCase("0, 0, 0", "0, 0, 1")]
         public void PlaneJson(string rootPoint, string unitVector)
         {
@@ -65,39 +63,15 @@
             Assert.AreEqual(plane, result);
         }
 
-        [Explicit("fix later")]
         [TestCase("1, 2, 3", "-1, 2, 3", false)]
         public void Ray3DJson(string ps, string vs, bool asElements)
         {
-            var ray = new Ray3D(Point3D.Parse(ps), UnitVector3D.Parse(vs));
+            var ray = new Ray3D(Point3D.Parse(ps), Vector3D.Parse(vs).Normalize());
             var result = this.JsonRoundTrip(ray);
             Assert.AreEqual(ray, result);
             AssertGeometry.AreEqual(ray, result, 1e-6);
         }
 
-        [Explicit("fix later")]
-        [TestCase("1, 2, 3", "4, 5, 6")]
-        public void Line3DJson(string p1s, string p2s)
-        {
-            Point3D p1 = Point3D.Parse(p1s);
-            Point3D p2 = Point3D.Parse(p2s);
-            var l = new Line3D(p1, p2);
-            var result = this.JsonRoundTrip(l);
-            Assert.AreEqual(l, result);
-        }
-
-        [Explicit("fix later")]
-        [TestCase("1, 2", "4, 5")]
-        public void Line2DJson(string p1s, string p2s)
-        {
-            Point2D p1 = Point2D.Parse(p1s);
-            Point2D p2 = Point2D.Parse(p2s);
-            var l = new Line2D(p1, p2);
-            var result = this.JsonRoundTrip(l);
-            Assert.AreEqual(l, result);
-        }
-
-        [Explicit("fix later")]
         [TestCase("1, 2, 3", "4, 5, 6")]
         public void LineSegment3DJson(string p1s, string p2s)
         {
@@ -108,7 +82,6 @@
             Assert.AreEqual(l, result);
         }
 
-        [Explicit("fix later")]
         [TestCase("1, 2", "4, 5")]
         public void LineSegment2DJson(string p1s, string p2s)
         {
@@ -119,7 +92,6 @@
             Assert.AreEqual(l, result);
         }
 
-        [Explicit("fix later")]
         [Test]
         public void Vector2DJson()
         {
@@ -128,7 +100,6 @@
             Assert.AreEqual(v, result);
         }
 
-        [Explicit("fix later")]
         [Test]
         public void Vector3DJson()
         {
@@ -146,7 +117,6 @@
             Assert.AreEqual(c, result);
         }
 
-        [Explicit("fix later")]
         [TestCase("0, 0, 0", 2.5)]
         public void Circle3DJson(string point, double radius)
         {
@@ -156,7 +126,6 @@
             Assert.AreEqual(c, result);
         }
 
-        [Explicit("fix later")]
         [Test]
         public void Polygon2DJson()
         {
@@ -166,7 +135,6 @@
             Assert.AreEqual(p, result);
         }
 
-        [Explicit("fix later")]
         [Test]
         public void PolyLine2DJson()
         {
@@ -176,7 +144,6 @@
             Assert.AreEqual(p, result);
         }
 
-        [Explicit("fix later")]
         [Test]
         public void PolyLine3DJson()
         {
@@ -186,19 +153,30 @@
             Assert.AreEqual(p, result);
         }
 
-        [Explicit("fix later")]
         [Test]
         public void CoordinateSystemJson()
         {
-            var cs = new CoordinateSystem(new Point3D(1, -2, 3), new Vector3D(0, 1, 0), new Vector3D(0, 0, 1), new Vector3D(1, 0, 0));
+            var cs = new Euclidean.CoordinateSystem(new Point3D(1, -2, 3), new Vector3D(0, 1, 0), new Vector3D(0, 0, 1), new Vector3D(1, 0, 0));
             var result = this.JsonRoundTrip(cs);
             AssertGeometry.AreEqual(cs, result);
         }
 
+        [Test]
+        public void UnitVector3DJson()
+        {
+            var uv = UnitVector3D.Create(0.2672612419124244, -0.53452248382484879, 0.80178372573727319);
+            var result = this.JsonRoundTrip(uv);
+            AssertGeometry.AreEqual(uv, result);
+        }
+
         private T JsonRoundTrip<T>(T test)
         {
-            string output = JsonConvert.SerializeObject(test);
-            return JsonConvert.DeserializeObject<T>(output);
+            JsonSerializerSettings settings = new JsonSerializerSettings();
+            settings.Converters.Add(new SpatialJsonConverter());
+            string output = JsonConvert.SerializeObject(test, settings);
+            Debug.WriteLine("json: " + output);
+            return JsonConvert.DeserializeObject<T>(output, settings);
         }
     }
+#endif
 }
