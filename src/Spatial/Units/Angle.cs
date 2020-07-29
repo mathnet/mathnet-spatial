e14bd9f15d0814442c434251af09423bd6c4104e
Rico Sinnig <38476230+rsinnig@users.noreply.github.com>
2020-07-29 10:35:31 +0200

    replace call to obsolete XmlExt by XmlWriteExt

--- src/Spatial/Units/Angle.cs
+++ src/Spatial/Units/Angle.cs
@@ -417,7 +417,7 @@ namespace MathNet.Spatial.Units
         /// <inheritdoc />
         void IXmlSerializable.WriteXml(XmlWriter writer)
         {
-            writer.WriteAttribute("Value", this.Radians);
+            writer.WriteElement("Value", this.Radians, "G15");
         }
     }
 }
