a410a9cd198dd4415c73066701a3202757b48b3d
jkalias <john@kaliakatsos.net>
2023-04-10 18:09:43 +0200

    Renamed Ray3DBenchmarks

--- src/Spatial.Benchmarks/LineBenchmarks.cs
+++ src/Spatial.Benchmarks/LineBenchmarks.cs
@@ -3,10 +3,10 @@ using MathNet.Spatial.Euclidean;
 
 namespace Spatial.Benchmarks
 {
-    public class Ray3DBenchmarks
+    public class LineBenchmarks
     {
-        private static readonly Line Ray3D1 = new Line(new Point3D(1, 2, 3), Direction.Create(1, 0, 0));
-        private static readonly Line Ray3D2 = new Line(new Point3D(3, 4, 5), Direction.Create(0, 1, 0));
+        private static readonly Line Line1 = new Line(new Point3D(1, 2, 3), Direction.Create(1, 0, 0));
+        private static readonly Line Line2 = new Line(new Point3D(3, 4, 5), Direction.Create(0, 1, 0));
         private static readonly Point3D Point3D = new Point3D(3, 4, 5);
         private static readonly Plane Plane = new Plane(1, 2, 3, 4);
         private static readonly Plane PlaneXy = new Plane(Point3D.Origin, Direction.ZAxis);
@@ -15,7 +15,7 @@ namespace Spatial.Benchmarks
         [Benchmark]
         public bool OperatorEqualityRay3DRay3D()
         {
-            return Ray3D1 == Ray3D2;
+            return Line1 == Line2;
         }
 
         [Benchmark]
@@ -27,31 +27,31 @@ namespace Spatial.Benchmarks
         [Benchmark]
         public LineSegment3D LineTo()
         {
-            return Ray3D1.ShortestLineSegmentTo(Point3D);
+            return Line1.ShortestLineSegmentTo(Point3D);
         }
 
         [Benchmark]
         public Point3D? IntersectionWith()
         {
-            return Ray3D1.IntersectionWith(Plane);
+            return Line1.IntersectionWith(Plane);
         }
 
         [Benchmark]
         public bool IsCollinear()
         {
-            return Ray3D1.IsCollinear(Ray3D2, 2);
+            return Line1.IsCollinear(Line2, 2);
         }
 
         [Benchmark]
         public bool Equals()
         {
-            return Ray3D1.Equals(Ray3D2);
+            return Line1.Equals(Line2);
         }
 
         [Benchmark]
         public bool EqualsWithTolerance()
         {
-            return Ray3D1.Equals(Ray3D2, 2);
+            return Line1.Equals(Line2, 2);
         }
     }
 }
