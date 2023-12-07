0e7eb693efa625b138e4a86bc2e74674fcc851ad
Ippei FURUHASHI <top.tuna+github@gmail.com>
2023-12-07 12:07:41 +0900

    fix: type declaration (from 1d to 1.0)

--- src/Spatial/Euclidean/Circle2D.cs
+++ src/Spatial/Euclidean/Circle2D.cs
@@ -167,7 +167,7 @@ namespace MathNet.Spatial.Euclidean
             var d = line.Direction;
             var r = this.Radius;
 
-            var a = 1d;
+            var a = 1.0;
             var b = 2 * (s.DotProduct(d) - d.DotProduct(cc));
             var c = (s - cc).DotProduct(s - cc) - r * r;
 
